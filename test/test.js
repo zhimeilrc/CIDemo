8f1b5348531a0c9384117074eb38f6567c92e9d0
Teddy Lin <lrc@zhimei360.com>
2016-02-18 15:50:44 +0800

    Use mocha test framework

--- test/test.js
+++ test/test.js
@@ -1,2 +1,13 @@
-console.log("hello world")
+var assert = require('assert');
+
+describe('Array', function(){
+	describe('#indexOf()', function(){
+		it('should return -1 when the value is not present', function () {
+			assert.equal(-1, [1,2,3].indexOf(5));
+		});
+		it('should return index value when the value is present', function () {
+			assert.equal(0, [1,2,3].indexOf(1));
+		});
+	});
+});
 
